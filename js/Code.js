d45d2206debad5df9f269c4d3c42b63a4667bc60
bre3ee <74628836+bre3ee@users.noreply.github.com>
2021-04-22 17:35:17 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U N I N T F O ", 700,150)
+    fill("lightblue")
+    text("Hint: Just start with F already", 700,170)  
+
+    fill("white")
+        textSize(15)
+        text("A S A D A B E T", 100,250)
+        fill("lightblue")
+        text("Hint: Stores info", 100,270)
+}
